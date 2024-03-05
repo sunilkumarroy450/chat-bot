9477ee1ad16f38ef22984bc303f3eb49bb3e0a5f
Sunil Kumar <sunilroy450@gmail.com>
2024-03-06 00:35:26 +0530

    feat: routes added to client side feat: some pages added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  return <>Hello</>;
+  return (
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<Signup />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
